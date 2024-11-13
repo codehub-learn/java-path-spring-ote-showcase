77b683e98eedf17de068738204f59c11bcaced76
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2024-11-13 12:56:43 +0200

    Removed forgotten sample code.

--- java-path-spring-app/src/main/java/gr/codelearn/spring/showcase/app/EshopApplication.java
+++ java-path-spring-app/src/main/java/gr/codelearn/spring/showcase/app/EshopApplication.java
@@ -1,6 +1,5 @@
 package gr.codelearn.spring.showcase.app;
 
-import gr.codelearn.spring.showcase.app.model.Customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,11 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EshopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EshopApplication.class, args);
-
-		var customer = new Customer();
-		customer.setEmail("some@gmailx.com");
-
-		var customerViaBuilder = Customer.builder().lastname("Someone").firstname("Me").build();
 
 	}
 }
